b81a171cc2aa6abc9fdbfb11afcf36be3c3c95c3
wteo <wendyteo.wy@gmail.com>
2022-02-06 10:24:10 +1100

    update Index JS file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fetchData = async (searchTerm) => {
         params: {
             apikey: "33c74cdf",
             s: searchTerm // axios allows you to include parameters in the second argument. instead of extension 
-            // If using thefetch method, you'll need to include the direct URL via "?[keyObject]=[value]&". 
+            // If using the fetch method, you'll need to include the direct URL via "?[keyObject]=[value]&". 
             // i.e http://www.omdbapi.com/?apikey=33c74cdf&s=avenger
             // At developer tool of Browser, find the URL by via Network > Fetch/XHR > fetched parameter > Header.
         }
@@ -14,25 +14,21 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector("input");
 
-/*
-input.addEventListener("input", (event) => {
-    fetchData(event.target.value); // this logs a new search every time a new key is pressed. 
-    // Not sufficient way of handling search as API only limits user to conduct 1000 searches per day.
-    // To overcome this issue, we set a timeout function.
-})
-*/
+const debounce = (callback, delay) => {
+    let timeoutId;
+    return (...arguments) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            callback.apply(null, arguments)}, 
+            // apply, calls function & takes all arguments & pass them as separate arguments to original function. 
+            delay);
+    }; // this function will be acting as the shield. a.k.a wrapper
+}
 
-
-let timeoutId;
 const onInput = (event) => {
-    if (timeoutId) {
-        clearTimeout(timeoutId);// first key press will skip this code. 
-        //On second press, statement will stop the current timer and reset to a new one.
-        // Process will happen over and over until a full second is called on the Timeout.
-    } 
-    timeoutId = setTimeout(() => {
-        fetchData(event.target.value);
-    }, 1000);
-};
+    fetchData(event.target.value);
+    };
 
-input.addEventListener("input", onInput);
+input.addEventListener("input", debounce(onInput, 500));
