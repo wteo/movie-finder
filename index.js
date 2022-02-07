0b44f2cdcfd2e71ef6b7670ee8d1f253a4c38319
wteo <wendyteo.wy@gmail.com>
2022-02-07 14:32:44 +1100

    Update files

--- index.js
+++ index.js
@@ -13,21 +13,35 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results">
+            </div>
+        </div>
+    </div>
+`;
 
 const input = document.querySelector("input");
-
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (event) => { 
     const movies = await fetchData(event.target.value); 
     
+    dropdown.classList.add("is-active");
     for (let movie of movies) {
-        const div = document.createElement("div");
+        const option = document.createElement("a");
 
-        div.innerHTML = `
-            <img src="${movie.Poster}"/>
-            <h1>${movie.Title}</h1>
-        `; // using back ticks so we can have multi-line String
-        document.querySelector("#target").appendChild(div);
+        option.classList.add("dropdown-item");
+        option.innerHTML = `
+            <img src="${movie.Poster}" />
+            ${movie.Title}
+        `; 
+        resultsWrapper.appendChild(option);
     }
     };
 
