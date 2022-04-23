c1d4a7083116672a4c7f1f31b160d83157bf7d61
wteo <wendyteo.wy@gmail.com>
2022-04-24 07:24:24 +1000

    added some test functions

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,66 @@
-it("works!!!", () => {
-    
+const waitFor = (selector) => {
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            if (document.querySelector(selector)) {
+                clearInterval(interval);
+                clearTimeout(timeout);
+                resolve();
+            }
+        }, 30);
+
+        const timeout = setTimeout(() => {
+            clearInterval(interval);
+            reject();
+        }, 2000);
+    });
+};
+
+beforeEach(() => {
+    document.querySelector("#target").innerHTML = "";
+    createAutoComplete({
+        root: document.querySelector("#target"),
+        fetchData() {
+            return [
+                { Title: "Avengers"}, 
+                { Title: "Avengers 2"},
+                { Title: "X Men"}
+            ];
+        },
+        renderOption(movie) {
+            return movie.Title;
+        }
+    });
+
+});
+
+it("Dropdown starts closed", () => {
+
+    const dropdown = document.querySelector(".dropdown");
+
+    expect(dropdown.className).not.to.include("is-active");
+
+});
+
+it("After searching, dropdown opens up", async () => {
+    const input = document.querySelector("input");
+    input.value = "avengers";
+    input.dispatchEvent(new Event("input")); // .dispatchEvent() automatically triggers an event
+
+    await waitFor(".dropdown-item");
+
+    const dropdown = document.querySelector(".dropdown");
+
+    expect(dropdown.className).to.include("is-active");
+});
+
+it("After searching, displays some results.", async () => {
+    const input = document.querySelector("input");
+    input.value = "avengers";
+    input.dispatchEvent(new Event("input")); // .dispatchEvent() automatically triggers an event
+
+    await waitFor(".dropdown-item");
+
+    const items = document.querySelectorAll(".dropdown-item");
+
+    expect(items.length).to.equal(3);
 });
